a345d2f9ef304f60779d21f9a8f8ac0ef65fb8e4
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2026-04-01 20:01:44 -0400

    Fix import mismatch.

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import CreateSurvey from './pages/createSurvey';
+import CreateSurvey from './pages/CreateSurvey';
 import SurveyView from './pages/SurveyView';
 import Analytics from './pages/Analytics';
 import SendInvitations from './pages/SendInvitations';
